c8cec6cb94ddf9a235e4fe7e6a68fa1332aa4c33
ec5104 <ec5104@nyu.edu>
2025-03-18 10:10:56 -0400

    push index code w/ music

--- script.js
+++ script.js
@@ -1,31 +1,14 @@
-let soundy, backy;
+document.addEventListener("DOMContentLoaded", function () {
+    let music = document.getElementById("background-music");
+    let toggleButton = document.getElementById("music-toggle");
 
-//checks to see fi the audio player is playing or not 
-function isPlaying(audelem){
-    return !audelem.paused;//returns true or false;
-}
-
-//wait until page and all html elements load
-window.onload = function(){
-    soundy = document.querySelector(".myAudio");
-    //soundy.preservePitch = false;
-    backy = document.querySelector(".colorBlock");
-    //executed the function called update every 10000 milisecond
-    setInterval(update(),1000);
-}
-
-function update(){
-if(isPlaying(soundy)){
-    soundy.playbackRate = (Math.random()*3.9) + .1;
-    }
-if(soundy.playbackRate > 2){
-    backy.style.backgroundColor = "red";
-
-}
-else{
-    backy.style.backgroundColor = "blue";
-
-}
-
-}
-
+    toggleButton.addEventListener("click", function () {
+        if (music.paused) {
+            music.play();
+            toggleButton.textContent = "Pause Music";
+        } else {
+            music.pause();
+            toggleButton.textContent = "Play Music";
+        }
+    });
+});
